90f760b380aecacf062333b9ca226c4f405e1270
Michael Dewberry <michael.dewberry@gmail>
2017-04-12 04:56:01 -0400

    Add health route

--- server/app.js
+++ server/app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var path = require('path');
 
-
 var videoSearch = require('./videoSearch.js');
 
 var app = express();
@@ -12,6 +11,16 @@ app.get('/video_search/:terms', function (req, res) {
   var searchTerms = req.params.terms;
   videoSearch.getTweets(searchTerms, function (urls) {
   	res.send(urls);
+  });
+});
+
+app.get('/health', function (req, res) {
+  videoSearch.getTweets("dance", function (urls, error) {
+  	if (urls.length > 0 && !error) {
+	  res.status(200).send({ serverUp: true, twitterUp: true });
+	} else {
+	  res.status(500).send({ serverUp: true, twitterUp: false, error: error });	
+	}
   });
 });
 
